fc7ff1fb54fa81a6488415d09d6064fe91eb927b
Sergey <yorke@inbox.ru>
2022-07-26 23:40:53 +0700

    censor filter edit

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -4,11 +4,25 @@ register = template.Library()
 
 BAD_WORDS = "редиска жопа писька"
 
+def is_int(text):
+    try:
+        int(text)
+        return True
+    except ValueError:
+        return False
+
+def is_float(text):
+    try:
+        float(text)
+        return True
+    except ValueError:
+        return False
+
 @register.filter()
 def censor(text):
     temp_text = text.split()
     for i, txt in enumerate(temp_text):
-        if isinstance(txt, str):
+        if not is_int(txt) and not is_float(txt):
             if txt.lower()[:-1] in BAD_WORDS:
                 temp_text[i] = f'{txt[0]}{"*" * (len(txt) - 1)}'
     return " ".join(temp_text)
